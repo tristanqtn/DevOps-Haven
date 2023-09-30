7e334896fce3da6cdde524faf74023df80ec215b
tristanqtn <tristan.querton@gmail.com>
2023-09-30 09:55:07 -0400

    feat: covered API with test

--- lab_3/src/routes/user.js
+++ lab_3/src/routes/user.js
@@ -1,29 +1,46 @@
-const express = require('express')
-const userController = require('../controllers/user')
+const express = require("express");
+const userController = require("../controllers/user");
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
 userRouter
-  .post('/', (req, resp) => {
+  .post("/", (req, resp) => {
     userController.create(req.body, (err, res) => {
-      let respObj
-      if(err) {
+      let respObj;
+      if (err) {
         respObj = {
           status: "error",
-          msg: err.message
-        }
-        return resp.status(400).json(respObj)
+          msg: err.message,
+        };
+        return resp.status(400).json(respObj);
       }
       respObj = {
         status: "success",
-        msg: res
-      }
-      resp.status(201).json(respObj)
-    })
+        msg: res,
+      };
+      return resp.status(201).json(respObj);
+    });
   })
-  // .get('/:username', (req, resp, next) => { // Express URL params - https://expressjs.com/en/guide/routing.html
-  //   // TODO Create get method API
-  //   const username = req.params.username
-  // })
-  
-module.exports = userRouter
+  .get("/:username", (req, resp, next) => {
+    // Express URL params - https://expressjs.com/en/guide/routing.html
+    //   // TODO Create get method API
+    const username = req.params.username;
+    userController.get(username, (err, res) => {
+      let respObj;
+      if (err) {
+        respObj = {
+          status: "error",
+          msg: err.message,
+        };
+        return resp.status(400).json(respObj);
+      }
+      console.log(res);
+      respObj = {
+        status: "success",
+        msg: res,
+      };
+      return resp.status(201).json(respObj);
+    });
+  });
+
+module.exports = userRouter;
